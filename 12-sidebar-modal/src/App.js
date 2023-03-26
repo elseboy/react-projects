ce7118c26f07735eadfaf6a41eb4dc51c3cd148d
elseboy <axuplus@gmail.com>
2023-03-26 18:06:40 +0800

    12-sidebar-modal

---
+++ 12-sidebar-modal/src/App.js
@@ -0,0 +1,16 @@
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+}
+
+export default App;
